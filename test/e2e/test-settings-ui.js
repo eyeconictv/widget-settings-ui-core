0930e1a4646cfc4d93fbcc79677581c2e17aab9b
Xiyang Chen <settinghead@gmail.com>
2014-06-25 03:06:51 -0400

    Test stubs

--- test/e2e/test-settings-ui.js
+++ test/e2e/test-settings-ui.js
@@ -1,22 +1,30 @@
 (function() {
 
-  "use strict";
+  'use strict';
 
   /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
-  var chai = require("chai");
-  var chaiAsPromised = require("chai-as-promised");
+  var chai = require('chai');
+  var chaiAsPromised = require('chai-as-promised');
 
   chai.use(chaiAsPromised);
   var expect = chai.expect;
 
-  var fs = require("fs");
+  var fs = require('fs');
 
   browser.driver.manage().window().setSize(1024, 768);
 
-  describe("Settings UI", function() {
-    it('Dummy test', function () {
+  describe('Settings UI', function() {
+    it('dummy test - DELETE ME', function () {
 
+    });
+
+    xit('Should correctly load settings', function () {
+      //TODO
+    });
+
+    xit('Should correctly save settings', function () {
+      //TODO
     });
   });
 })();
--- test/unit/svc-settings-spec.js
+++ test/unit/svc-settings-spec.js
@@ -11,5 +11,56 @@ describe('Settings UI', function() {
         done();
       });
     });
+
+    xit('should save parameters', function () {
+      //TODO
+    });
+
+    xit('should save additional parameters', function () {
+      //TODO
+    });
+  });
+
+  describe('settingsGetter', function () {
+    it('should exist', function(done) {
+      inject(function (settingsGetter) {
+        expect(settingsGetter).be.defined;
+        done();
+      });
+    });
+
+    xit('should get parameters', function () {
+      //TODO
+    });
+
+    xit('should get additional parameters', function () {
+      //TODO
+    });
+
+  });
+
+  describe('settingsParser', function () {
+    it('should exist', function(done) {
+      inject(function (settingsParser) {
+        expect(settingsParser).be.defined;
+        done();
+      });
+    });
+
+    xit('should parse params string', function () {
+      //TODO
+    });
+
+    xit('should encode params to string', function () {
+      //TODO
+    });
+
+    xit('should parse additional params string', function () {
+      //TODO
+    });
+
+    xit('should encode additional params to string', function () {
+      //TODO
+    });
   });
 });
