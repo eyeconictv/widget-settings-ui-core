63573ad87a76b0dbf6d8535e4c86b0b4165a6d21
Donna Murphy <donna.peplinskie@risevision.com>
2014-10-07 10:25:38 -0400

    Fix issue with saving params

--- src/js/svc-settings.js
+++ src/js/svc-settings.js
@@ -102,7 +102,8 @@ angular.module("risevision.widget.common")
           str.push("up_" + encodeURIComponent(p) + "=" + encodeURIComponent(value));
         }
       }
-      return "?" + str.join("&");
+
+      return str.join("&");
     };
 
     function stripPrefix(name) {
