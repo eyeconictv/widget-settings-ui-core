cf7d13b2d626d68c71b3516c7d27661f5b610d1d
Donna Murphy <donna.peplinskie@risevision.com>
2015-01-30 13:39:12 -0500

    Remove hasValidExtensions from imageValidator

--- dist/widget-settings-ui-core.js
+++ dist/widget-settings-ui-core.js
@@ -124,17 +124,6 @@ angular.module("risevision.widget.common")
 angular.module("risevision.widget.common")
   .factory("imageValidator", ["$q", function ($q) {
     var factory = {
-      hasValidExtension: function(url) {
-        var extensions = [".jpg", ".jpeg", ".png", ".bmp", ".svg", ".gif"];
-
-        for (var i = 0, len = extensions.length; i < len; i++) {
-          if (url.indexOf(extensions[i]) !== -1) {
-            return true;
-          }
-        }
-
-        return false;
-      },
       // Verify that URL is a valid image file.
       isImage: function(src) {
         var deferred = $q.defer(),
--- test/unit/svc-image-validator-spec.js
+++ test/unit/svc-image-validator-spec.js
@@ -15,33 +15,5 @@ describe("Image Validator", function() {
     it("should exist", function() {
       expect(imageValidator).to.be.defined;
     });
-
-    it("should return true for a .jpg file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image.jpg")).to.be.true;
-    });
-
-    it("should return true for a .jpeg file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image.jpeg")).to.be.true;
-    });
-
-    it("should return true for a .png file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image.png")).to.be.true;
-    });
-
-    it("should return true for a .bmp file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image.bmp")).to.be.true;
-    });
-
-    it("should return true for a .svg file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image.svg")).to.be.true;
-    });
-
-    it("should return true for a .gif file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image.gif")).to.be.true;
-    });
-
-    it("should return false for non-image file", function() {
-      expect(imageValidator.hasValidExtension("http://fakeurl.com/my-image")).to.be.false;
-    });
   });
 });
