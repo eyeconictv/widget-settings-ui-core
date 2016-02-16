1b9d578e18b02769af9356ae9b61fdc2bdb4e4c2
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-02-16 15:21:56 -0500

    Add googleFontLoader factory

--- test/e2e/gadgets-api-mock.js
+++ test/e2e/gadgets-api-mock.js
@@ -1,17 +1,17 @@
 window.result = {
     additionalParams: JSON.stringify(
       {
-        'textSetting':'hello world',
-        'checkboxSetting':true,
-        'textAreaSetting': 'the quick brown fox jumps over the lazy dog'
+        "textSetting":"hello world",
+        "checkboxSetting":true,
+        "textAreaSetting": "the quick brown fox jumps over the lazy dog"
       }
     ),
-    params: '?up_textSetting1=defg'
+    params: "?up_textSetting1=defg"
   };
 
 window.gadgets = {
   rpc: function (methodName, callback, params) {
-    if(methodName === 'rscmd_saveSettings') {
+    if(methodName === "rscmd_saveSettings") {
       window.result.params = params.params;
       window.result.additionalParams = params.additionalParams;
       if(callback) {
@@ -19,7 +19,7 @@ window.gadgets = {
       }
       else{ return params; }
     }
-    else if (methodName === 'rscms_closeSettings') {
+    else if (methodName === "rscms_closeSettings") {
       if(callback) {
         callback(true);
       }
@@ -27,11 +27,11 @@ window.gadgets = {
         return true;
       }
     }
-    else if (methodName === 'rscmd_getAdditionalParams'){
+    else if (methodName === "rscmd_getAdditionalParams"){
       if(callback) {
         callback(window.result.additionalParams);
       }
     }
-    else {throw 'Unknown method'; }
+    else {throw "Unknown method"; }
   }
 };
--- test/e2e/settings-ui-scenarios.js
+++ test/e2e/settings-ui-scenarios.js
@@ -1,52 +1,53 @@
+/* global element: false, browser: false, by: false */
 (function() {
 
-  'use strict';
+  "use strict";
 
   /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
-  var chai = require('chai');
-  var chaiAsPromised = require('chai-as-promised');
+  var chai = require("chai");
+  var chaiAsPromised = require("chai-as-promised");
 
   chai.use(chaiAsPromised);
   var expect = chai.expect;
   browser.driver.manage().window().setSize(1024, 768);
 
-  describe('Settings UI', function() {
+  describe("Settings UI", function() {
     beforeEach(function (){
-      browser.get('/test/e2e/test.html?up_id=hello&up_textSetting1=abc&windowSize=200');
+      browser.get("/test/e2e/test.html?up_id=hello&up_textSetting1=abc&windowSize=200");
     });
 
-    it('Should correctly load settings', function () {
-      element(by.id('loadSettings')).click();
-      expect(element(by.id('textSettingInParams')).getAttribute('value')).to.eventually.equal('abc');
-      expect(element(by.id('textSetting')).getAttribute('value')).to.eventually.equal('hello world');
-      expect(element(by.id('checkboxSetting')).getAttribute('checked')).to.eventually.equal('true');
-      expect(element(by.id('textAreaSetting')).getText()).to.eventually.equal('the quick brown fox jumps over the lazy dog');
+    it("Should correctly load settings", function () {
+      element(by.id("loadSettings")).click();
+      expect(element(by.id("textSettingInParams")).getAttribute("value")).to.eventually.equal("abc");
+      expect(element(by.id("textSetting")).getAttribute("value")).to.eventually.equal("hello world");
+      expect(element(by.id("checkboxSetting")).getAttribute("checked")).to.eventually.equal("true");
+      expect(element(by.id("textAreaSetting")).getText()).to.eventually.equal("the quick brown fox jumps over the lazy dog");
     });
 
-    it('Should correctly save settings', function (done) {
-      element(by.id('textSettingInParams')).clear().sendKeys('defg');
-      element(by.id('textSetting')).clear().sendKeys('bye world');
-      element(by.id('checkboxSetting')).click();
-      element(by.id('textAreaSetting')).clear().sendKeys('There is nothing either good or bad but thinking makes it so.');
-      element(by.id('saveSettings')).click();
-      expect(browser.executeScript('return window.result')).to.eventually.deep.equal(
+    it("Should correctly save settings", function () {
+      element(by.id("textSettingInParams")).clear().sendKeys("defg");
+      element(by.id("textSetting")).clear().sendKeys("bye world");
+      element(by.id("checkboxSetting")).click();
+      element(by.id("textAreaSetting")).clear().sendKeys("There is nothing either good or bad but thinking makes it so.");
+      element(by.id("saveSettings")).click();
+      expect(browser.executeScript("return window.result")).to.eventually.deep.equal(
         {
-          'additionalParams': '{"textSetting":"bye world","checkboxSetting":false,"textAreaSetting":"the quick brown fox jumps over the lazy dog"}',
-          'params': 'up_textSetting1=defg'
+          "additionalParams": "{\"textSetting\":\"bye world\",\"checkboxSetting\":false,\"textAreaSetting\":\"the quick brown fox jumps over the lazy dog\"}",
+          "params": "up_textSetting1=defg"
         });
     });
 
-    it('Should correctly close settings', function (done) {
-      element(by.id('textSettingInParams')).clear().sendKeys('defg');
-      element(by.id('textSetting')).clear().sendKeys('bye world');
-      element(by.id('checkboxSetting')).click();
-      element(by.id('textAreaSetting')).clear().sendKeys('There is nothing either good or bad but thinking makes it so.');
-      element(by.id('closeSettings')).click();
-      expect(browser.executeScript('return window.result')).to.eventually.deep.equal(
+    it("Should correctly close settings", function () {
+      element(by.id("textSettingInParams")).clear().sendKeys("defg");
+      element(by.id("textSetting")).clear().sendKeys("bye world");
+      element(by.id("checkboxSetting")).click();
+      element(by.id("textAreaSetting")).clear().sendKeys("There is nothing either good or bad but thinking makes it so.");
+      element(by.id("closeSettings")).click();
+      expect(browser.executeScript("return window.result")).to.eventually.deep.equal(
         {
-          'additionalParams': '{"textSetting":"hello world","checkboxSetting":true,"textAreaSetting":"the quick brown fox jumps over the lazy dog"}',
-          'params': '?up_textSetting1=defg'
+          "additionalParams": "{\"textSetting\":\"hello world\",\"checkboxSetting\":true,\"textAreaSetting\":\"the quick brown fox jumps over the lazy dog\"}",
+          "params": "?up_textSetting1=defg"
         });
     });
   });
